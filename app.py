b636e62d8f866b4c15583b5b880eaa0ecab18cc0
IrinaSergevna <ir.saldaeva@gmail.com>
2025-02-10 20:02:26 +0300

    app.py исправлен

--- app.py
+++ app.py
@@ -1,33 +1,46 @@
+# Импорт встроенной библиотеки для работы веб-сервером
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import json
 
-# Для начала определим настройки запуска
-hostName = "localhost" # Адрес для доступа по сети
-serverPort = 8080 # Порт для доступа по сети
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
 
 class MyServer(BaseHTTPRequestHandler):
     """
-        Специальный класс, который отвечает за
-        обработку входящих запросов от клиентов
+    Специальный класс, который отвечает за
+    обработку входящих запросов от клиентов
     """
 
     def get_context_data(self):
+        """Метод для чтения HTML-файла"""
         with open("contact.html", "r", encoding="utf-8") as file:
             context = file.read()
         return context
 
     def do_GET(self):
         """ Метод для обработки входящих GET-запросов """
-        content = self.get_context_data()
-        self.send_response(200) # Отправка кода ответа
-        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
-        self.end_headers() # Завершение формирования заголовков ответа
-        self.wfile.write(bytes(content, "utf-8")) # Тело ответа
+        self.send_response(200)  # Отправка кода ответа
+        self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+        self.end_headers()  # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(self.get_context_data(), "utf-8"))  # Тело ответа с содержимым HTML-файла
+
+    def do_POST(self):
+        """ Метод для обработки входящих POST-запросов """
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)  # Чтение данных запроса
+        print(body.decode("utf-8"))  # Декодируем байты в строку для вывода на экран
+        response = {'message': 'Data received'}  # Ответ в формате JSON
+        self.send_response(200)  # Отправка кода ответа
+        self.send_header("Content-type", "application/json")  # Указание типа данных
+        self.end_headers()  # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(json.dumps(response), "utf-8"))  # Тело ответа в формате JSON
 
 if __name__ == "__main__":
     # Инициализация веб-сервера, который будет по заданным параметрам в сети
-    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    # принимать запросы и отправлять их на обработку специальному классу MyServer
     webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+    print(f"Server started http://{hostName}:{serverPort}")
 
     try:
         # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
